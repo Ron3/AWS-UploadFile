8207f1c87d5b02b093facd68b90af34394bd9a40
Ron3 <luodm03@qq.com>
2017-03-14 12:13:09 +0800

    -change: 修改启动脚本.

--- sh/start_http_server.sh
+++ sh/start_http_server.sh
@@ -5,14 +5,12 @@
 source $HOME/.bash_profile
 workon bpsg
 
-cd ../
-
 if [ -f ../tmp/bs2Http.pid ]
 then
     kill `cat ../tmp/bs2Http.pid`
 fi
 
-cd ../server
+cd ../
 twistd --pidfile=../tmp/bs2Http.pid bs2err --port 7777
 
 
